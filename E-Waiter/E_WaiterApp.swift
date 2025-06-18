a9a9e6da3d6c11d9401b1b0996eea3a63630f112
AppLabStudios <youssef.azroun.applab@gmail.com>
2025-06-18 03:36:20 +0200

    Initial Commit

---
+++ E-Waiter/E_WaiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_WaiterApp.swift
+//  E-Waiter
+//
+//  Created by Youssef Azroun on 2025-06-18.
+//
+
+import SwiftUI
+
+@main
+struct E_WaiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ E-WaiterTests/E_WaiterTests.swift
@@ -0,0 +1,17 @@
+//
+//  E_WaiterTests.swift
+//  E-WaiterTests
+//
+//  Created by Youssef Azroun on 2025-06-18.
+//
+
+import Testing
+@testable import E_Waiter
+
+struct E_WaiterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
